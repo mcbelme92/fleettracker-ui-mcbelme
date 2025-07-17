e1d95f8bee323332f72fb09570438a1437dff312
pepe tellez <mcbelme92@gmail.com>
2025-07-16 23:21:23 -0600

    fix/se soluciona la persistencia de datos ya no ahce fetch al hacer f5

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -3,4 +3,4 @@ export const VEHICLES_ENDPOINT = `${API_BASE_URL}/vehicles`;
 export const VEHICLE_STORAGE_KEY = "persisted_vehicles";
 export const VEHICLE_PAGE_KEY = "VEHICLE_PAGE_KEY";
 export const VEHICLE_LIMIT_KEY = "VEHICLE_LIMIT_KEY";
-export const HAS_FETCHED_KEY = "HAS_FETCHED_KEY";
+export const VEHICLE_TOTAL_KEY = "VEHICLE_TOTAL_KEY";
